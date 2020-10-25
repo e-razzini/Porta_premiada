c96375511daf6a1f84390cbec1fa238a5841f8ec
Elton <eltonrazzini@hotmail.com>
2020-10-25 19:21:12 -0300

    porta premiada

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>problema monty hall</h1>
+    <Door numero="1" :hasGift="false" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Door from "./components/Door";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: { Door },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  font-family: "Titillium Web", sans-serif;
+}
+body {
+  color: #fff;
+  background: linear-gradient(
+    90deg,
+    rgba(180, 58, 93, 1) 10%,
+    rgba(253, 71, 29, 0.9575980050223214) 50%,
+    rgba(252, 97, 69, 1) 75%
+  );
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#app h1 {
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
 }
 </style>
---
+++ src/components/Door.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="door-area">
+
+<div class="door-frame" :class="{selecionada: selecionada && !aberta}">
+
+    <Gift v-if ="aberta && hasGift"/>
+
+</div>
+
+<div class="door" :class="{aberta}" @click="selecionada = !selecionada">
+
+    <div class="numero" :class="{selecionada}">{{numero}}</div>
+    <div class="knob" :class="{selecionada}" @click="aberta = true"></div>
+
+</div>
+
+  </div>
+</template>
+
+<script>
+
+import Gift from './Gift';
+
+export default {
+
+name:'Door',
+components: {Gift},
+
+props: {
+    numero:{},
+    hasGift :{type: Boolean}
+},
+data: function(){
+    return {
+        aberta: false,
+        selecionada :false
+        
+    }
+}
+
+}
+</script>
+
+<style>
+:root {
+    --door-border : 5px solid brown;
+    --selecionada-borda:5px solid yellow;
+}
+.door-area {
+    position: relative;
+    width: 200px;
+    height: 310px;
+    border-bottom: 10px solid #AAA;
+    margin-bottom:20px;
+    font-size: 3rem;
+
+    display: flex;
+    justify-content: center;
+
+}
+
+.door-frame {
+    position: absolute;
+    height: 300px;
+    width: 180px;
+
+    border-left: var(--door-border);
+    border-top: var(--door-border);
+    border-right: var(--door-border);
+
+   display: flex;
+   justify-content: center;
+   align-items: flex-end;
+}
+.door {
+    position: absolute;
+    top: 5px;
+    height: 295px;
+    width: 170px;
+    background-color: chocolate;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+}
+.door .knob {
+    height: 20px;
+    width: 20px;
+    border-radius:10px;
+    background-color:brown;
+    align-self: flex-start;
+    margin-top:60px;
+
+}
+.door-frame.selecionada {
+    border-left:var(--selecionada-borda);
+    border-top:var(--selecionada-borda);
+    border-right:var(--selecionada-borda)
+}
+.door .numero.selecionada {
+   color:yellow;
+   }
+.door .knob.selecionada {
+   background-color: yellow;
+}
+.door.aberta {
+    background-color: #0007;
+}
+.door.aberta .knob {
+    display: none;
+}
+.door.aberta .numero {
+    display: none;
+}
+
+</style>
